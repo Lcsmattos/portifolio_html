c88471e8c8d484e9cb5ba79fb89de8630ff45681
Lucas Mattos <lcsmattos7@gmail.com>
2024-06-04 13:09:25 -0300

    incluindo animação no scroll da página

---
+++ reveal.js
@@ -0,0 +1,102 @@
+
+window.revelar = ScrollReveal({ reset: true })
+
+//Topo do site
+
+revelar.reveal('.efeito-txt-topo', {
+  duration: 2000,
+  distance: '90px'
+})
+
+revelar.reveal('.efeito-img-topo', {
+  duration: 2000,
+  distance: '90px',
+  delay: 300
+})
+
+// //Titulos do site
+
+revelar.reveal('.titulo', {
+  duration: 2000,
+  distance: '90px'
+})
+
+// //Sessão Especialidades
+
+revelar.reveal('.efeito-especialidades-1', {
+  duration: 2000,
+  distance: '90px',
+  delay: 500
+})
+
+revelar.reveal('.efeito-especialidades-2', {
+  duration: 2000,
+  distance: '90px',
+  delay: 1000
+})
+
+revelar.reveal('.efeito-especialidades-3', {
+  duration: 2000,
+  distance: '90px',
+  delay: 1500
+})
+
+// //Sessão Sobre
+
+revelar.reveal('.efeito-img-sobre', {
+  duration: 2000,
+  distance: '90px',
+  origin: 'right',
+  delay: 500
+})
+
+revelar.reveal('.efeito-txt-sobre', {
+  duration: 2000,
+  distance: '90px',
+  origin: 'left',
+  delay: 1000
+})
+
+// //Sessão Download 
+
+revelar.reveal('.efeito-download-btn', {
+  duration: 2000,
+  distance: '90px',
+  delay: 500,
+})
+
+// //Sessão Formulário
+
+revelar.reveal('.efeito-input-1', {
+  duration: 2000,
+  distance: '90px',
+  delay: 500,
+})
+revelar.reveal('.efeito-input-2', {
+  duration: 2000,
+  distance: '90px',
+  delay: 750,
+})
+revelar.reveal('.efeito-input-3', {
+  duration: 2000,
+  distance: '90px',
+  delay: 1000,
+})
+revelar.reveal('.efeito-input-4', {
+  duration: 2000,
+  distance: '90px',
+  delay: 1250,
+})
+
+revelar.reveal('.efeito-input-5', {
+  duration: 2000,
+  distance: '90px',
+  delay: 1250,
+})
+
+revelar.reveal('.efeito-disable-txt', {
+  duration: 2000,
+  distance: '90px',
+  delay: 1250,
+})
+
